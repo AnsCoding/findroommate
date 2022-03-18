a99a5697c3b4aceba37f60d87cf761829cb3bb6f
Ajay-Parajuli <83760685+Ajay-Parajuli@users.noreply.github.com>
2022-03-18 10:58:35 +0100

    Firebase setup - linked

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -2,13 +2,15 @@ import { initializeApp } from "firebase/app";
 import { getFirestore, collection } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBAOu3NqaUza7vzDQli9dYRaUHwLZldiWw",
-    authDomain: "race-react-firebase.firebaseapp.com",
-    projectId: "race-react-firebase",
-    storageBucket: "race-react-firebase.appspot.com",
-    messagingSenderId: "2118672388",
-    appId: "1:2118672388:web:06a1d58d898e9702581551"
+  apiKey: "AIzaSyBN8fTsJ79KBpMqGJwH7X6Yisg0JRujFSE",
+  authDomain: "roommate-29e88.firebaseapp.com",
+  projectId: "roommate-29e88",
+  storageBucket: "roommate-29e88.appspot.com",
+  messagingSenderId: "218841412128",
+  appId: "1:218841412128:web:1ad17d7f7f437568a1c245",
+  measurementId: "G-8KR09HF38Q",
 };
+
 const firebaseApp = initializeApp(firebaseConfig);
 const db = getFirestore(firebaseApp);
 
